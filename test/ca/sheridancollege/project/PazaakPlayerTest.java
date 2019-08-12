5bc1362143fa2ccb1ab64336d83bc03a1047195b
jammoo <dinoman0624@hotmail.com>
2019-08-12 00:24:40 -0400

    Implemented some of the test cases for PazaakPlayer, and removed test methods for getters and setters

--- test/ca/sheridancollege/project/PazaakPlayerTest.java
+++ test/ca/sheridancollege/project/PazaakPlayerTest.java
@@ -51,55 +51,14 @@ public class PazaakPlayerTest {
     }
 
     /**
-     * Test of getCredits method, of class PazaakPlayer.
-     */
-    @Test
-    public void testGetCredits() {
-        System.out.println("getCredits");
-        PazaakPlayer instance = null;
-        int expResult = 0;
-        int result = instance.getCredits();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setCredits method, of class PazaakPlayer.
-     */
-    @Test
-    public void testSetCredits() {
-        System.out.println("setCredits");
-        int credits = 0;
-        PazaakPlayer instance = null;
-        instance.setCredits(credits);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
      * Test of stand method, of class PazaakPlayer.
      */
     @Test
     public void testStand() {
         System.out.println("stand");
-        PazaakPlayer instance = null;
+        PazaakPlayer instance = new PazaakPlayer("John");
         instance.stand();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setStanding method, of class PazaakPlayer.
-     */
-    @Test
-    public void testSetStanding() {
-        System.out.println("setStanding");
-        boolean standing = false;
-        PazaakPlayer instance = null;
-        instance.setStanding(standing);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(instance.isStanding(), true);
     }
 
     /**
@@ -108,12 +67,10 @@ public class PazaakPlayerTest {
     @Test
     public void testIsStanding() {
         System.out.println("isStanding");
-        PazaakPlayer instance = null;
+        PazaakPlayer instance = new PazaakPlayer("John");
         boolean expResult = false;
         boolean result = instance.isStanding();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -122,23 +79,9 @@ public class PazaakPlayerTest {
     @Test
     public void testEndTurn() {
         System.out.println("endTurn");
-        PazaakPlayer instance = null;
+        PazaakPlayer instance = new PazaakPlayer("John");
         instance.endTurn();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setTurnOver method, of class PazaakPlayer.
-     */
-    @Test
-    public void testSetTurnOver() {
-        System.out.println("setTurnOver");
-        boolean turnOver = false;
-        PazaakPlayer instance = null;
-        instance.setTurnOver(turnOver);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(instance.isTurnOver(), true);
     }
 
     /**
@@ -147,24 +90,33 @@ public class PazaakPlayerTest {
     @Test
     public void testIsTurnOver() {
         System.out.println("isTurnOver");
-        PazaakPlayer instance = null;
+        PazaakPlayer instance = new PazaakPlayer("John");
+        instance.play(new PazaakGame());
         boolean expResult = false;
         boolean result = instance.isTurnOver();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
      * Test of forfeit method, of class PazaakPlayer.
      */
     @Test
-    public void testForfeit() {
+    public void testForfeitCardTotal() {
         System.out.println("forfeit");
-        PazaakPlayer instance = null;
+        PazaakPlayer instance = new PazaakPlayer("John");
         instance.forfeit();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(instance.getCardTotal(), -1);
+    }
+    
+    /**
+     * Test of forfeit method, of class PazaakPlayer.
+     */
+    @Test
+    public void testForfeitTurnOver() {
+        System.out.println("forfeit");
+        PazaakPlayer instance = new PazaakPlayer("John");
+        instance.forfeit();
+        assertEquals(instance.isTurnOver(), true);
     }
 
     /**
@@ -173,119 +125,10 @@ public class PazaakPlayerTest {
     @Test
     public void testPlayCard() {
         System.out.println("playCard");
-        SideCard card = null;
-        PazaakPlayer instance = null;
+        SideCard card = new SideCard(SideValue.PLUS_TWO);
+        PazaakPlayer instance = new PazaakPlayer("John");
         instance.playCard(card);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getHand method, of class PazaakPlayer.
-     */
-    @Test
-    public void testGetHand() {
-        System.out.println("getHand");
-        PazaakPlayer instance = null;
-        GroupOfCards expResult = null;
-        GroupOfCards result = instance.getHand();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setHand method, of class PazaakPlayer.
-     */
-    @Test
-    public void testSetHand() {
-        System.out.println("setHand");
-        GroupOfCards hand = null;
-        PazaakPlayer instance = null;
-        instance.setHand(hand);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getWins method, of class PazaakPlayer.
-     */
-    @Test
-    public void testGetWins() {
-        System.out.println("getWins");
-        PazaakPlayer instance = null;
-        int expResult = 0;
-        int result = instance.getWins();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setWins method, of class PazaakPlayer.
-     */
-    @Test
-    public void testSetWins() {
-        System.out.println("setWins");
-        int wins = 0;
-        PazaakPlayer instance = null;
-        instance.setWins(wins);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getCardTotal method, of class PazaakPlayer.
-     */
-    @Test
-    public void testGetCardTotal() {
-        System.out.println("getCardTotal");
-        PazaakPlayer instance = null;
-        int expResult = 0;
-        int result = instance.getCardTotal();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setCardTotal method, of class PazaakPlayer.
-     */
-    @Test
-    public void testSetCardTotal() {
-        System.out.println("setCardTotal");
-        int cardTotal = 0;
-        PazaakPlayer instance = null;
-        instance.setCardTotal(cardTotal);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getTableHand method, of class PazaakPlayer.
-     */
-    @Test
-    public void testGetTableHand() {
-        System.out.println("getTableHand");
-        PazaakPlayer instance = null;
-        GroupOfCards expResult = null;
-        GroupOfCards result = instance.getTableHand();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setTableHand method, of class PazaakPlayer.
-     */
-    @Test
-    public void testSetTableHand() {
-        System.out.println("setTableHand");
-        GroupOfCards tableHand = null;
-        PazaakPlayer instance = null;
-        instance.setTableHand(tableHand);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(2, instance.getCardTotal());
     }
 
     /**
@@ -294,11 +137,10 @@ public class PazaakPlayerTest {
     @Test
     public void testAddTableCard() {
         System.out.println("addTableCard");
-        TableCard card = null;
-        PazaakPlayer instance = null;
+        TableCard card = new TableCard(TableValue.EIGHT);
+        PazaakPlayer instance = new PazaakPlayer("John");
         instance.addTableCard(card);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(8, instance.getCardTotal());
     }
     
 }
