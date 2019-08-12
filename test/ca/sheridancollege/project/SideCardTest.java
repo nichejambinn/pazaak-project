a63c2611eabc201d9e3b97e6bf6570cd109c354c
jammoo <dinoman0624@hotmail.com>
2019-08-11 23:46:56 -0400

    added another method to test file for SideCard.java

--- test/ca/sheridancollege/project/SideCardTest.java
+++ test/ca/sheridancollege/project/SideCardTest.java
@@ -37,9 +37,19 @@ public class SideCardTest {
     }
 
     @Test
-    public void testToString() {
+    public void testToStringPositive() {
         SideCard a = new SideCard(SideValue.PLUS_ONE);
         String expResult = " ____ \n|+1  |\n|    |\n|____|\n";
+        String result = a.toString();
+        System.out.println(expResult);
+        System.out.println(result);
+        assertEquals(expResult, result);
+    };
+    
+    @Test
+    public void testToStringNegative() {
+        SideCard a = new SideCard(SideValue.MINUS_FIVE);
+        String expResult = " ____ \n|-5  |\n|    |\n|____|\n";
         String result = a.toString();
         System.out.println(expResult);
         System.out.println(result);
